b6c2812eed059f481452661f1090b15da1e6aee5
ADsglt77 <adrien.segalat@gmail.com>
2026-02-15 17:58:22 +0100

    📄 - (page) Clean ProjectsPage

--- frontend/src/pages/ProjectsPage.vue
+++ frontend/src/pages/ProjectsPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Button from "../components/Button.vue";
-import { iconExpand } from "../data/icons";
 import ProjectCard from "../components/ProjectCard.vue";
+import { iconExpand } from "../data/icons";
 import { projects } from "../data/projects";
 </script>
 
